3b74a48b509c4c8c8db393cd91adc5387499c6ab
Ankita Maharjan <maharjanankita655@gmail.com>
2024-01-17 07:43:06 +0545

    fix lab 2

--- lab-3/lab-3.cpp
+++ lab-3/lab-3.cpp
@@ -1,69 +1,70 @@
 #include<iostream>
 using namespace std;
 
+
 // function declarations
-void q0(string, int, int); // final state
-void q1(string, int, int);
-void q2(string, int, int);
-void q3(string, int, int);
+void q0(string, int); // final state
+void q1(string, int);
+void q2(string, int);
+void q3(string, int);
 
-// function definitions
-void q0(string s, int i, int diff) { // final state
+
+// function definations
+void q0(string s, int i) { // final state
     if (i == s.size()) {
-        cout << "The string is ";
-        if (diff == 0) {
-            cout << "accepted!\n";
-        } else {
-            cout << "not accepted!\n";
-        }
+        cout << "The string is accepted!\n";
         return;
     }
 
     if (s[i] == 'a') {
-        q1(s, i+1, diff + 1);
+        q1(s, i+1);
     } else if (s[i] == 'b') {
-        q2(s, i+1, diff - 1);
+        q2(s, i+1);
     }
 }
 
-void q1(string s, int i, int diff) {
+
+void q1(string s, int i) {
     if (i == s.size()) {
         cout << "The string is not accepted!\n";
         return;
     }
 
     if (s[i] == 'a') {
-        q0(s, i+1, diff);
+        q0(s, i+1);
     } else if (s[i] == 'b') {
-        q3(s, i+1, diff - 1);
+        q3(s, i+1);
     }
 }
 
-void q2(string s, int i, int diff) {
+
+void q2(string s, int i) {
     if (i == s.size()) {
         cout << "The string is not accepted!\n";
         return;
     }
 
     if (s[i] == 'a') {
-        q3(s, i+1, diff + 1);
+        q3(s, i+1);
     } else if (s[i] == 'b') {
-        q0(s, i+1, diff);
+        q0(s, i+1);
     }
 }
 
-void q3(string s, int i, int diff) {
+
+void q3(string s, int i) {
     if (i == s.size()) {
         cout << "The string is not accepted!\n";
         return;
     }
 
     if (s[i] == 'a') {
-        q2(s, i+1, diff - 1);
+        q2(s, i+1);
     } else if (s[i] == 'b') {
-        q1(s, i+1, diff);
+        q1(s, i+1);
     }
 }
+
 
 int main() {
     string str;
@@ -71,7 +72,7 @@ int main() {
     cout << "Enter the string: ";
     cin >> str;
 
-    q0(str, 0, 0);
+    q0(str, 0);
 
     return 0;
 }
